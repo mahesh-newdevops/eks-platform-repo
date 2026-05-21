beb0e0938799c48ec6bffc029965de0c50d4ecef
Jatinsai <maheshambarapu@gmail.com>
2026-05-21 13:03:19 +0000

    Automate create/destroy workflow; fix S3 backend region and add safe cleanup

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket         = "eks-terraform-state-prod"
     key            = "eks/terraform.tfstate"
-    region         = "ap-south-1"
+    region         = "us-west-2"
     dynamodb_table = "terraform-locks"
     encrypt        = true
   }
